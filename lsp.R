b2531a0de182ae734f44eb5af1b252b45488fae3
datascience <ShahHome2015@gmail.com>
2018-09-28 14:55:15 -0500

    Functions in a Package

---
+++ lsp.R
@@ -0,0 +1,9 @@
+lsp <- function(package, all.names = FALSE, pattern) 
+{
+  package <- deparse(substitute(package))
+  ls(
+    pos = paste("package", package, sep = ":"), 
+    all.names = all.names, 
+    pattern = pattern
+  )
+}
